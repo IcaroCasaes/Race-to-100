f12f8d399439c61e7591874b5ff9dfc1276027eb
Icaro Casaes <icaro_casaes@hotmail.com>
2021-12-21 17:47:55 -0300

    Organizing img
    New folder storing separately all img

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ btnRoll.addEventListener('click', function () {
 
     // 2. Display it
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `img/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
